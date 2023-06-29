01550c5369dcf42bb3753108f282fd239b9aed3f
alfygitau <alfygitau@gmail.com>
2023-06-29 12:52:40 +0300

    fixed ordercard, testimonial

---
+++ src/components/navbar/Navigation.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import "./Navigation.css";
+import { NavLink } from "react-router-dom";
+import SettingsPhoneIcon from "@mui/icons-material/SettingsPhone";
+
+const Navigation = () => {
+  return (
+    <div className="main-navigation-container">
+      <div className="main-navigation">
+        <div className="nav-content">
+          <div className="logo">
+            <div className="navbar-image">
+              <img
+                src="https://icon-library.com/images/security-icon-png/security-icon-png-2.jpg"
+                alt="logo"
+              />
+            </div>
+            <span>Deluxe Essays</span>
+          </div>
+          <div className="links">
+            <NavLink to="/services">Services</NavLink>
+            <NavLink to="/about-us">About Us</NavLink>
+            <NavLink to="/reviews">Reviews</NavLink>
+            <NavLink to="/pricing">Pricing</NavLink>
+            <NavLink to="/sample-papers">Samples</NavLink>
+            <NavLink to="/contact-us">Contact Us</NavLink>
+            <NavLink to="/faq">FAQ</NavLink>
+          </div>
+          <div className="auth">
+            <div>
+              <SettingsPhoneIcon />
+            </div>
+            <button className="button-order">Order Now</button>
+            <button className="button-login">Login</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navigation;
